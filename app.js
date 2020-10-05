f31b7c3d7d8a5d5200edad9388b48f85cac2d8b9
Meat <47371524+rawagschal@users.noreply.github.com>
2020-10-05 01:04:50 -0700

    install inquirer

--- app.js
+++ app.js
@@ -1,39 +1,37 @@
-//use built-in node.js function require() to include core fs module
-const fs = require('fs');
-//require the generatePage module after exporting in page-template.js
-const generatePage = require('./src/page-template.js');
 
-// creates an array to hold user command-line arguments
-const profileDataArgs = process.argv.slice(2);
+const inquirer = require('inquirer');
 
-const [name, github] = profileDataArgs
+//use the inquirer.prompt method to access the 
+//question object, which is an array of more objects)
+inquirer
+    .prompt ([
+       {
+           type: 'input',
+           name: 'name',
+           message: 'What is your name?'
+       } 
+    ])
+    //answer object is returned as a Promise
+    .then(answers => console.log(answers));
+// //use built-in node.js function require() to include core fs module
+// const fs = require('fs');
+// //require the generatePage module after exporting in page-template.js
+// const generatePage = require('./src/page-template');
 
-//asynchronous function's allow the computer to run the process in the background,
-//freeing up resources w/out halting runtime
-fs.writeFile('./index.html', generatePage(name, github), err => {
-   //callback to check if error exists, creating an exception
-   //and thus stopping the code execution
-    if (err) throw new Error(err);
-    //will concatenate the 'name' and 'message' text properties of the Error object
-    //good to know for retreiving Error properties in development
-
-    //direct user to inspect newly created file
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// // creates an array to hold user command-line arguments
+// const pageHTML = generatePage(name, github);
 
 
+// //asynchronous functions allow the computer to run the process in the background,
+// //freeing up resources w/out halting runtime
+// fs.writeFile('./index.html', pageHTML, err => {
+//    //callback to check if error exists, creating an exception
+//    //and thus stopping the code execution
+//     if (err) throw (err);
+//     //will concatenate the 'name' and 'message' text properties of the Error object
+//     //good to know for retreiving Error properties in development
 
-// console.log(profileDataArgs);
+//     //direct user to inspect newly created file
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-// const printProfileData = profileDataArr => {
-//     // this...
-//     for (let i = 0; i < profileDataArr.length; i+= 1){
-//         console.log(profileDataArr[i]);
-//     }
-//     console.log('=========');
-
-//     //is the same as this...
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
